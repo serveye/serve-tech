3f23c187e00713470d2ddf644ed8db923f8894e8
Ramakotaiah Vadlamudi <ramakotaiah12@gmail.com>
2021-04-13 09:59:45 +1000

    feat: Install and configure top level eslint
    > updated the configuration files
    >removed the package.json from top level

--- packages/tech-admin/webpack.config.js
+++ packages/tech-admin/webpack.config.js
@@ -1,40 +1,52 @@
-const path = require("path");
-const HWP = require("html-webpack-plugin");
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HWP = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "production",
+    mode: 'production',
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: 'source-map',
 
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "ts-loader",
-          },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                    },
+                ],
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: 'source-map-loader',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
         ],
-      },
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader",
-      },
+    },
+    plugins: [
+        new HWP({
+            template: path.resolve(__dirname, 'index.html'),
+            filename: 'index.html',
+            inject: 'body',
+        }),
     ],
-  },
-  plugins: [
-    new HWP({
-      template: path.resolve(__dirname, "index.html"),
-      filename: "index.html",
-      inject: "body",
-    }),
-  ],
 };
--- packages/tech-provider/src/index.tsx
+++ packages/tech-provider/src/index.tsx
@@ -1,21 +1,22 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
 
-import ComponentLibrary from 'tech-components'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import ComponentLibrary from 'tech-components';
 
 export interface HelloProps { name: string; }
 
 
-class Hello extends React.Component<HelloProps, {}> {
+class Hello extends React.Component<HelloProps, string> {
     render() {
         return (   <>
-        <h1>Hello from {this.props.name}!</h1>;
-        <ComponentLibrary />
-        </>)
+            <h1>Hello from {this.props.name}!</h1>;
+            <ComponentLibrary />
+        </>);
     }
 }
 
 ReactDOM.render(
     <Hello name="Application" />,
-    document.getElementById("example")
+    document.getElementById('example')
 );
