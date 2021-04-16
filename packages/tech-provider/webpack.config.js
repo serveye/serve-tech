53c7f346f2604304415fd04a4a4a1d68fa019eba
Ramakotaiah Vadlamudi <ramakotaiah12@gmail.com>
2021-04-16 10:53:53 +1000

    feat: Install and configure antd and scss
    > Install antd and scss
    >configure webpack for scss

--- packages/tech-provider/webpack.config.js
+++ packages/tech-provider/webpack.config.js
@@ -29,6 +29,17 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'source-map-loader',
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
         ],
     },
     plugins: [
